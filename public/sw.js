bd5a5041813b5e43a64c41c735b261aa188934c3
yasinarafatajad <yasinarafatazad173@gmail.com>
2024-11-05 18:28:03 +0600

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // let cacheData = "appV1";
 
 // this.addEventListener("install", (event) = {
@@ -42,48 +41,3 @@
 // )
 
 // })
-=======
-let cacheData = "appV1";
-
-this.addEventListener("install", (event) = {
-
-event.waitUntil(
-
-caches.open(cacheData).then((cache) {
-
-cache.addAll(
-
-'/static/js/main.chunk.js',
-
-'/static/js/0.chunk.js',
-
-'/static/js/bundle.js',
-
-'/index.html',
-
-})
-
-)
-
-})
-
-this.addEventListener("fetch", (event)=>{
-
-event.respondWith(
-
-caches.match(event.request).then((resp)=>{
-
-if(resp)
-
-{
-
-return resp
-
-}
-
-})
-
-)
-
-})
->>>>>>> c268942f062a5cc327008eac044af0c8dba3d3a6
