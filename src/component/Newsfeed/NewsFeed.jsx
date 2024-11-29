1d1ce96a4510a772aa8c8afb464377e9dfb30b3f
yasinarafatajad <yasinarafatazad173@gmail.com>
2024-11-30 03:32:53 +0600

    check err

--- src/component/Newsfeed/NewsFeed.jsx
+++ src/component/Newsfeed/NewsFeed.jsx
@@ -18,7 +18,7 @@ const NewsFeed = () => {
       <div className="container">
         <div className="notice border mb-4">
           <div className="timer">
-          <h1 className="text-center"> It's going to happen A big change here.</h1>
+          <h1 className="text-center"> Its going to happen A big change here.</h1>
           <div id="flip_timer"></div>
           </div>
         </div>
